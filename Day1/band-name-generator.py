201344b3d05aa1418c589c9c2038859bff07fa87
Niall Swan <46677948+niallswan@users.noreply.github.com>
2024-03-07 03:06:17 +0000

    Basic band name generator for day 1 complete

---
+++ Day1/band-name-generator.py
@@ -0,0 +1,11 @@
+#Greeting
+print("Welcome to my band name generator!")
+
+#Prompt the user to enter city they grew up in
+cityName = input("What city did you grow up in?\n")
+
+#Prompt the user for the name of their pet
+petName = input("What is the name of a pet you have owned?\n")
+
+#Combine names
+print("The name of your band is: " + cityName + " " + petName + "!")
